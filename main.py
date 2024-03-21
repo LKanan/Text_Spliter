a35553a91dd3031f3bda7bf6289d8bb13edb0ef4
LKanan <lucienkanani@gmail.com>
2024-03-22 00:31:01 +0200

    Changement du nom de la fonction de split en spliter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def split(text, separator):
+def spliter(text, separator):
     text = str(text)
     word = text.split(str(separator))
     print(word)
 
-split("Hello word", " ")
+spliter("Hello word", " ")
